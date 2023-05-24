e98888a74efe50f73007102f7bd93bea50e62620
Lumina <alexander.andrejeff@elev.ga.lbs.se>
2023-05-24 14:44:11 +0200

    Gun fixed

--- Assets/Scripts/Player/Gun.cs
+++ Assets/Scripts/Player/Gun.cs
@@ -1,52 +1,78 @@
 #region
-using System.Collections;
 using UnityEngine;
+using static Essentials.Attributes;
 #endregion
 
 public class Gun : MonoBehaviour
 {
-  [SerializeField] Camera FPCamera;
-  [SerializeField] float range = 100f;
-  [SerializeField] int damage = 35;
-  float magazine;
-  [SerializeField] float maxMagazine = 18;
-  [SerializeField] float fireRate = 1.88f;
+    [SerializeField] float range = 100f;
+    [SerializeField] int damage = 35;
+    [SerializeField] float fireRate = 1.88f;
+    [SerializeField] float maxMagazineSize = 18;
 
-  bool allowFire = true;
+    [SerializeField, ReadOnly] float currentMagazine;
 
-  public float FireRate
-  {
-  get => fireRate;
-    set => fireRate = value;
-  }
+    bool allowFire = true;
+    Camera playerCam;
 
-  public float Magazine
-  {
-    get => maxMagazine;
-    set => maxMagazine = value;
-  }
-
-  void Start()
-  {
-    magazine = maxMagazine;
-  }
-
-  void Update()
-  {
-    if (Input.GetButtonDown("Fire1") && magazine > 0 && allowFire)
+    public float FireRate
     {
-      StartCoroutine(Shoot());
+        get => fireRate;
+        set => fireRate = value;
     }
-    
-  IEnumerator Shoot()
-  {
-    magazine--;
-    RaycastHit hit;
-    if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
 
-    allowFire = false;
-    yield return new WaitForSeconds(FireRate/60);
-    allowFire = true;
+    public float MaxMagazineSize
+    {
+        get => maxMagazineSize;
+        set => maxMagazineSize = value;
     }
-    
+
+    void Start()
+    {
+        // Get the camera component from the child of this object.
+        playerCam = transform.parent.GetChild(0).GetComponent<Camera>();
+
+        // Set the current magazine to the maximum size.
+        currentMagazine = maxMagazineSize;
+    }
+
+    void Update()
+    {
+        // "Fire1" == Left mouse button.
+        if (Input.GetButtonDown("Fire1") && currentMagazine > 0 && allowFire)
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        currentMagazine--;
+
+        // Shoots
+        StartCoroutine(Essentials.Sequencing.SequenceActions(() =>
+        {
+            allowFire = false;
+            Debug.Log("allowFire = false");
+
+            // Raycast to a distance of 100 units and debug the name of the object hit.
+            if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out RaycastHit hit, range))
+            {
+                Debug.Log($"Hit: {hit.transform.name}");
+
+                if (hit.collider != null)
+                {
+                    // Do on-hit logic, such as damaging the enemy etc.
+                    // Get the health component from the object hit.
+                    //Health target = hit.transform.GetComponent<Health>();
+                }
+            }
+
+        }, FireRate, () =>
+        {
+            // Shoot has finished, perform clean up actions.
+            allowFire = true;
+            Debug.Log("allowFire = true");
+        }));
+    }
 }
