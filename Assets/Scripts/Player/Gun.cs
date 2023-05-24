c8d8705a91e79a9e29b7af92c5e3267854e48b17
Lumina <alexander.andrejeff@elev.ga.lbs.se>
2023-05-24 15:54:41 +0200

    Health & Gun changes

--- Assets/Scripts/Player/Gun.cs
+++ Assets/Scripts/Player/Gun.cs
@@ -1,18 +1,22 @@
 #region
+using Essentials;
 using UnityEngine;
 using static Essentials.Attributes;
+using static Interfaces;
 #endregion
 
 public class Gun : MonoBehaviour
 {
+    [Header("Gun Settings")]
     [SerializeField] float range = 100f;
+    [SerializeField] float fireRate;
     [SerializeField] int damage = 35;
-    [SerializeField] float fireRate = 1.88f;
-    [SerializeField] float maxMagazineSize = 18;
 
-    [SerializeField, ReadOnly] float currentMagazine;
+    [Header("Read-Only Fields")]
+    [SerializeField, ReadOnly] bool canFire;
 
-    bool allowFire = true;
+    // Cached References.
+    Magazine magazine;
     Camera playerCam;
 
     public float FireRate
@@ -21,58 +25,49 @@ public class Gun : MonoBehaviour
         set => fireRate = value;
     }
 
-    public float MaxMagazineSize
-    {
-        get => maxMagazineSize;
-        set => maxMagazineSize = value;
-    }
-
     void Start()
     {
-        // Get the camera component from the child of this object.
-        playerCam = transform.parent.GetChild(0).GetComponent<Camera>();
+        canFire  = true;
+        magazine = GetComponent<Magazine>();
 
         // Set the current magazine to the maximum size.
-        currentMagazine = maxMagazineSize;
+        magazine.CurrentMagSize = magazine.MaxMagazineSize;
+
+        // Get the camera component from the child of this object.
+        playerCam = transform.parent.GetChild(0).GetComponent<Camera>();
     }
 
     void Update()
     {
         // "Fire1" == Left mouse button.
-        if (Input.GetButtonDown("Fire1") && currentMagazine > 0 && allowFire)
-        {
-            Shoot();
-        }
+        if (Input.GetButton("Fire1") && magazine.CurrentMagSize > 0 && canFire) Shoot();
     }
 
     void Shoot()
     {
-        currentMagazine--;
+        magazine.CurrentMagSize--;
 
         // Shoots
-        StartCoroutine(Essentials.Sequencing.SequenceActions(() =>
+        StartCoroutine(Sequencing.SequenceActions(() =>
         {
-            allowFire = false;
-            Debug.Log("allowFire = false");
+            canFire = false;
 
             // Raycast to a distance of 100 units and debug the name of the object hit.
             if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out RaycastHit hit, range))
             {
                 Debug.Log($"Hit: {hit.transform.name}");
 
-                if (hit.collider != null)
-                {
+                // Get the health component from the object hit.
+                if (hit.transform.TryGetComponent(out IDamageable component))
                     // Do on-hit logic, such as damaging the enemy etc.
-                    // Get the health component from the object hit.
-                    //Health target = hit.transform.GetComponent<Health>();
-                }
+                    component.TakeDamage(damage);
             }
 
+            // FireRate == the time between each shot.
         }, FireRate, () =>
         {
             // Shoot has finished, perform clean up actions.
-            allowFire = true;
-            Debug.Log("allowFire = true");
+            canFire = true;
         }));
     }
 }
---
+++ Assets/Scripts/Player/Magazine.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using static Essentials.Attributes;
+
+public class Magazine : MonoBehaviour
+{
+    [SerializeField] float maxMagSize = 18;
+    [SerializeField, ReadOnly] float currentMagSize;
+
+    public float MaxMagazineSize
+    {
+        get => maxMagSize;
+        set => maxMagSize = value;
+    }
+
+    public float CurrentMagSize
+    {
+        get => currentMagSize;
+        set
+        {
+            currentMagSize = value;
+
+            if (currentMagSize <= 0)
+            {
+                Debug.Log("Out of ammo! (Can't fire) \n Reload to continue firing.");
+            }
+        }
+    }
+}
