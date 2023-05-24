c35ba6358e2812abd1bcb291065be0dcac81034a
Ministar200 <128044449+Ministar200@users.noreply.github.com>
2023-05-24 13:53:43 +0200

    Picking up gun
    gun picking up gun

--- Assets/Scripts/Player/Gun.cs
+++ Assets/Scripts/Player/Gun.cs
@@ -1,5 +1,4 @@
-using System;
-using System.Security.Cryptography;
+using System.Collections;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
@@ -7,22 +6,47 @@ public class Gun : MonoBehaviour
   [SerializeField] Camera FPCamera;
   [SerializeField] float range = 100f;
   [SerializeField] int damage = 35;
+  float magazine;
+  [SerializeField] float maxMagazine = 18;
+  [SerializeField] float fireRate = 1.88f;
+
+  bool allowFire = true;
+
+  public float FireRate
+  {
+  get => fireRate;
+    set => fireRate = value;
+  }
+
+  public float Magazine
+  {
+    get => maxMagazine;
+    set => maxMagazine = value;
+  }
+
+  void Start()
+  {
+    magazine = maxMagazine;
+  }
 
   void Update()
   {
-    if (Input.GetButtonDown("Fire1"))
+    if (Input.GetButtonDown("Fire1") && magazine > 0 && allowFire)
     {
-      Shoot();
+      StartCoroutine(Shoot());
     }
   }
 
-  void Shoot()
+  IEnumerator Shoot()
   {
+    magazine--;
     RaycastHit hit;
-
     if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
     {
       Debug.Log("I hit this thing" + hit.transform.name);
     }
+    allowFire = false;
+    yield return new WaitForSeconds(FireRate/60);
+    allowFire = true;
   }
 }
