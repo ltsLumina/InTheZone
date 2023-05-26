aca9cb5b757a8db88f0727e8ce4220ba6a39569c
Rave068 <068.rave@gmail.com>
2023-05-26 11:18:03 +0200

    Further improved enemy spawner
    Now gradually increases amount of enemies spawning

--- Assets/Scripts/EnemySpawnerGroundcheck.cs
+++ Assets/Scripts/EnemySpawnerGroundcheck.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawnerGroundcheck : MonoBehaviour
+public class EnemySpawnerGroundcheck : EnemySpawner
 {
     [SerializeField] LayerMask groundLayer;
     [SerializeField] GameObject[] enemyTypes;
 
-    EnemySpawner enemySpawner;
-
     void Start()
     {
-        enemySpawner = FindObjectOfType<EnemySpawner>();
-
         // spawns an enemy once raycast hits ground layer
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity, groundLayer))
